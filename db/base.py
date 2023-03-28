180a7a4239323ddc8c1c083b7eed1981913e10ac
Владислав <kant1755@mail.ru>
2023-03-28 22:48:39 +0300

    databases

---
+++ db/base.py
@@ -0,0 +1,2 @@
+from databases import Database
+from sqlalchemy import create_engine, MetaData
--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = FastAPI()
 async def root():
     return {'messange': 'Hello World!'} 
 
+@app.on_event('startup')
+async def startup():
+    await
+
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, host='0.0.0.0', reload=True)
