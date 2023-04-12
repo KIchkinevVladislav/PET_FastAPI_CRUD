7a784d6db440b914171f3618474027b9fcb0732a
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-04-12 22:53:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,6 @@ async def startup():
 async def shutdown():
     await databese.disconnect()
 
+    
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, host='0.0.0.0', reload=True)
